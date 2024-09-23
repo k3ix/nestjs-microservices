208080636b7efb539f5ab17a37d104deccf17a5b
yahor-mishuk <yahor.mishuk@humandev.org>
2024-09-23 14:31:15 +0400

    feat: split env, auth app

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,12 +1,10 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config';
-import { ConfigModule } from '@app/common';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [forwardRef(() => ConfigModule)],
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGODB_URI'),
       }),
