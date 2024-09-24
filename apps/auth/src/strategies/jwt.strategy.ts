bcebb0142fb07ce40bbd7bdc0b86228a11ec2ed5
yahor-mishuk <yahor.mishuk@humandev.org>
2024-09-24 10:24:18 +0400

    feat: auth as microservice

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -2,7 +2,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../users/users.service';
-import { Request } from 'express';
 import { ITokenPayload } from '../interfaces/token-payload.interface';
 import { Injectable } from '@nestjs/common';
 
@@ -14,7 +13,9 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => request?.cookies?.Authentication,
+        (request: any) => {
+          return request?.cookies?.Authentication || request?.Authentication;
+        },
       ]),
       secretOrKey: configService.get('JWT_SECRET'),
     });
---
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,31 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { map, Observable, tap } from 'rxjs';
+import { ClientProxy } from '@nestjs/microservices';
+import { AUTH_SERVICE } from '../constants/services';
+import { UserDto } from '@app/common/dto';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
+    if (!jwt) {
+      throw new UnauthorizedException();
+    }
+    return this.authClient.send('authenticate', { Authentication: jwt }).pipe(
+      tap((res: UserDto) => {
+        context.switchToHttp().getRequest().user = res;
+      }),
+      map(() => true),
+    );
+  }
+}
